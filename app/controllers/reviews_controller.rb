54064887cde16f7c74a47c619ad993ebdfb5af23
Trouni Tiet <trouni@gmail.com>
2019-05-22 17:29:31 +0900

    moved review creation to restaurant show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,7 +15,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "restaurants/show.html.erb"
     end
   end
 
